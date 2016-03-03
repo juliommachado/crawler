6b9223d0009a24d4b57e17bbf30edee319d3723a
Rubico <bruno-santiago@users.noreply.github.com>
2016-03-03 17:04:58 +0530

    Add the Dispatcher class

---
+++ Dispatcher.py
@@ -0,0 +1,17 @@
+__author__= 'rubico'
+
+
+class Dispatcher:
+    pool = None
+    
+    def __init__(self, *args, **kwargs):
+        self.pool = []
+        
+    def has_work(self):
+        return len(self.pool) is not 0
+        
+    def get_work(self):
+        return self.pool.pop(0)
+        
+    def fill_pool(self, workload):
+        self.pool = self.pool + workload
