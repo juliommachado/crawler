6a8e0f2166cccbe9f0514c98b59fc5e78f5bf0d2
Rubico <bruno-santiago@users.noreply.github.com>
2016-03-03 17:05:59 +0530

    Added the fetcher class

---
+++ Fetcher.py
@@ -0,0 +1,31 @@
+__author__ = 'rubico'
+
+from threading import Thread
+import time
+
+
+class Fetcher(Thread):
+    
+    dispatcher = None
+    work = None
+    
+    def __init__(self, dispatcher, *args, **kwargs):
+        Thread.__init__(self, *args, **kwargs)
+        #TODO (rubico) - see if the dispatcher is a istance of Dispatcher or any subclass
+        self.dispatcher = dispatcher
+        self.start()
+        
+    def get_work(self):
+        self.work = None
+        if dispatcher.has_work():
+            self.work = dispatcher.get_work()
+            
+    def do(self):
+        if self.work is not None:
+            print self.work
+        
+    def run(self):
+        while True:
+            time.sleep(3)
+            self.get_work()
+            self.do()
