27250a8cf6aeff14a8325800debcec8513b94e19
= <brunosantiagopinheiro@gmail.com>
2016-03-14 14:07:15 +0530

    Added UrlDownload and UrlDownloadManager

--- Models/UrlDownload.py
+++ Models/UrlDownload.py
@@ -1,1 +1,36 @@
 __author__ = 'rubico'
+
+from Models import UrlDownloadManager
+from urlparse import urlparse, urljoin
+
+
+class UrlDownload:
+
+    manager = UrlDownloadManager
+
+    ID_POSITION = 0
+    URL_POSITION = 1
+    IS_DOWNLOADED_POSITION = 2
+
+    def __init__(self, url=None, is_downloaded=None, url_master=None, *args, **kwargs):
+        if kwargs.has_key('tuple'):
+            self.id = kwargs['tuple'][UrlDownload.ID_POSITION]
+            self.url = self.__set_url(self, url, url_master)
+            self.is_downloaded = is_downloaded
+
+    def save(self):
+        self.manager.save(self)
+
+    def get_host(self):
+        parsed_uri = urlparse(self.url)
+        return parsed_uri.netloc
+
+    def is_relative(self):
+        return self.get_host() == ''
+
+    def __set_url(self, url, url_master):
+        self.url = url
+        if self.is_relative():
+            self.url = urljoin(url_master, self.url)
+
+
--- Models/UrlDownloadManager.py
+++ Models/UrlDownloadManager.py
@@ -1,1 +1,36 @@
+from Database.Connection import Connection
+import Settings
+
 __author__ = 'rubico'
+
+
+class UrlDownloadManager:
+
+    __connection = Connection(Settings.sqllite_file_location)
+
+    @staticmethod
+    def get_by_id(id):
+        return UrlDownloadManager.__connection.execute('SELECT id, url, is_downloaded FROM UrlDownload WHERE id ?', (id,))
+
+    @staticmethod
+    def get_by_url(url):
+        return UrlDownloadManager.__connection.execute('SELECT id, url, is_downloaded FROM UrlDownload WHERE url LIKE ?', (url,))
+
+    @staticmethod
+    def exists(url):
+        result = UrlDownloadManager.__connection.execute('SELECT id FROM UrlDownload WHERE url like ?', (url,))
+        return result is not None
+
+    @staticmethod
+    def save(url_download_object):
+        database_url = UrlDownloadManager.get_by_url(url_download_object.url)
+        if database_url is not None:
+            UrlDownloadManager.__connection.execute(
+                'UPDATE UrlDownload SET url = ?, is_downloaded = ? WHERE id = ?',
+                (url_download_object.url, url_download_object.is_downloaded, database_url)
+            )
+        else:
+            UrlDownloadManager.__connection.execute(
+                'INSERT INTO UrlDownload(url, is_downloaded) VALUES (?, ?)',
+                (url_download_object.url, False)
+            )
