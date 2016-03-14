b2c137f18ca761b705d17df8632e33e2ddc3682b
= <brunosantiagopinheiro@gmail.com>
2016-03-14 17:15:49 +0530

    Dispatcher is only a queue now, no access to database

--- Dispatcher.py
+++ Dispatcher.py
@@ -9,19 +9,6 @@ class Dispatcher:
     
     def __init__(self, *args, **kwargs):
         self.pool = []
-        self.connection = kwargs.get('connection', None)
-        if self.connection is None:
-            raise Exception('Dispatcher needs a connection to work properly')
-        results = None
-        try:
-            results = self.__get_pending_pool()
-        except sqlite3.OperationalError:
-            print 'Dispatcher didn\'t load any url, Database on lock'
-
-        if results is not None:
-            for result in results:
-                url = Url(tuple=result)
-                self.pool.append(url)
         
     def has_work(self):
         return len(self.pool) is not 0
@@ -31,6 +18,3 @@ class Dispatcher:
         
     def fill_pool(self, workload):
         self.pool = self.pool + workload
-
-    def __get_pending_pool(self):
-        return Url.manager.get_pending_urls()
